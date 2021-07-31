355372a6a831b16f79de01651931e3aaec5a3cab
bobi1539 <ahmadrival833@gmail.com>
2021-07-31 12:19:11 +0700

    membuat entity kendaraan dengan nama Vehicle.java

---
+++ src/main/java/zero/programmer/data/kendaraan/entities/Vehicle.java
@@ -0,0 +1,161 @@
+package zero.programmer.data.kendaraan.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vehicles")
+public class Vehicle implements Serializable{
+
+    @Id
+    @Column(name = "registration_number", length = 30)
+    private String registrationNumber;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String merk;
+
+    @Column(name = "chassis_number", unique = true)
+    private String chassisNumber;
+
+    @Column(name = "manchine_number", unique = true)
+    private String machineNumber;
+
+    @Column(name = "police_number", unique = true)
+    private String policeNumber;
+
+    @Column(name = "purchase_date")
+    private Date purchaseDate;
+
+    @Column(name = "acquisition_value")
+    private Long acquisitionValue;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "condition_vehicle")
+    private String condition;
+
+    @Column(name = "borrow_status")
+    private String borrowStatus;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String registrationNumber, String name, String merk, String chassisNumber, String machineNumber,
+            String policeNumber, Date purchaseDate, Long acquisitionValue, String location, String condition,
+            String borrowStatus) {
+        this.registrationNumber = registrationNumber;
+        this.name = name;
+        this.merk = merk;
+        this.chassisNumber = chassisNumber;
+        this.machineNumber = machineNumber;
+        this.policeNumber = policeNumber;
+        this.purchaseDate = purchaseDate;
+        this.acquisitionValue = acquisitionValue;
+        this.location = location;
+        this.condition = condition;
+        this.borrowStatus = borrowStatus;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getChassisNumber() {
+        return chassisNumber;
+    }
+
+    public void setChassisNumber(String chassisNumber) {
+        this.chassisNumber = chassisNumber;
+    }
+
+    public String getMachineNumber() {
+        return machineNumber;
+    }
+
+    public void setMachineNumber(String machineNumber) {
+        this.machineNumber = machineNumber;
+    }
+
+    public String getPoliceNumber() {
+        return policeNumber;
+    }
+
+    public void setPoliceNumber(String policeNumber) {
+        this.policeNumber = policeNumber;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Long getAcquisitionValue() {
+        return acquisitionValue;
+    }
+
+    public void setAcquisitionValue(Long acquisitionValue) {
+        this.acquisitionValue = acquisitionValue;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getBorrowStatus() {
+        return borrowStatus;
+    }
+
+    public void setBorrowStatus(String borrowStatus) {
+        this.borrowStatus = borrowStatus;
+    }
+
+    
+
+
+
+    
+}
