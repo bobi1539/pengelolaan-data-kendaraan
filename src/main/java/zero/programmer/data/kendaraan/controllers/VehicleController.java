c09d4fe0b0d0d21d4b47c4318738a1bf6d9daaf8
bobi1539 <ahmadrival833@gmail.com>
2021-08-01 13:28:49 +0700

    membuat function update dan createOrUpdate untuk save data ke database di VehicleController dan memperbaiki function untuk create

--- src/main/java/zero/programmer/data/kendaraan/controllers/VehicleController.java
+++ src/main/java/zero/programmer/data/kendaraan/controllers/VehicleController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,39 +23,24 @@ import zero.programmer.data.kendaraan.services.VehicleService;
 @RestController
 @RequestMapping("/api/vehicle")
 public class VehicleController {
-    
+
     @Autowired
     private VehicleService vehicleService;
 
-    @PostMapping(
-        produces = MediaType.APPLICATION_JSON_VALUE,
-        consumes = MediaType.APPLICATION_JSON_VALUE
-    )
-    public ResponseEntity<ResponseData<VehicleData>> create(@Valid @RequestBody VehicleData vehicleData, Errors errors){
-        ResponseData<VehicleData> responseData = new ResponseData<>();
-        if (errors.hasErrors()){
-            for (ObjectError error : errors.getAllErrors()){
-                responseData.getMessages().add(error.getDefaultMessage());
-            }
-            responseData.setCode(400);
-            responseData.setStatus("BAD REQUEST");
-            responseData.setData(null);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
-        }
-        responseData.setCode(200);
-        responseData.setStatus("OK");
-        responseData.getMessages().add("Data berhasil ditambahkan");
-        responseData.setData(vehicleService.create(vehicleData));
-        return ResponseEntity.ok().body(responseData);
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseData<VehicleData>> create(@Valid @RequestBody VehicleData vehicleData,
+            Errors errors) {
+        return createOrUpdate(vehicleData, errors, "Data berhasil ditambahkan");
     }
 
     @GetMapping("/{registrationNumber}")
-    public ResponseEntity<ResponseData<VehicleData>> getVehicle(@PathVariable("registrationNumber") String registrationNumber){
+    public ResponseEntity<ResponseData<VehicleData>> getVehicle(
+            @PathVariable("registrationNumber") String registrationNumber) {
 
         ResponseData<VehicleData> responseData = new ResponseData<>();
 
         VehicleData vehicleData = vehicleService.getVehicle(registrationNumber);
-        if (vehicleData == null){
+        if (vehicleData == null) {
             responseData.setCode(404);
             responseData.setStatus("NOT FOUND");
             responseData.getMessages().add("Data tidak ditemukan");
@@ -65,6 +51,30 @@ public class VehicleController {
         responseData.setStatus("OK");
         responseData.setMessages(null);
         responseData.setData(vehicleData);
+        return ResponseEntity.ok().body(responseData);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ResponseData<VehicleData>> update(@Valid @RequestBody VehicleData vehicleData,
+            Errors errors) {
+        return createOrUpdate(vehicleData, errors, "Data berhasil diubah");
+    }
+
+    private ResponseEntity<ResponseData<VehicleData>> createOrUpdate(VehicleData vehicleData, Errors errors, String messages) {
+        ResponseData<VehicleData> responseData = new ResponseData<>();
+        if (errors.hasErrors()) {
+            for (ObjectError error : errors.getAllErrors()) {
+                responseData.getMessages().add(error.getDefaultMessage());
+            }
+            responseData.setCode(400);
+            responseData.setStatus("BAD REQUEST");
+            responseData.setData(null);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+        }
+        responseData.setCode(200);
+        responseData.setStatus("OK");
+        responseData.getMessages().add(messages);
+        responseData.setData(vehicleService.create(vehicleData));
         return ResponseEntity.ok().body(responseData);
     }
 
